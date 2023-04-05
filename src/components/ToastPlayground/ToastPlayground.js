0afd75d2696d56c3e171692e164ab613b1b0da35
Vitali Rotteker <vitali.rotteker@posteo.de>
2023-04-05 23:20:06 +0200

    Complete Exercise 2: Live-editable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -4,12 +4,14 @@ import Button from "../Button";
 
 import styles from "./ToastPlayground.module.css";
 import { useState } from "react";
+import Toast from "../Toast/Toast";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = useState("");
   const [variant, setVariant] = useState("notice");
+  const [isToastVisible, setIsToastVisible] = useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -17,6 +19,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isToastVisible && (
+        <Toast variant={variant} handleDismiss={() => setIsToastVisible(false)}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -63,7 +71,13 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button
+              onClick={() => {
+                setIsToastVisible(true);
+              }}
+            >
+              Pop Toast!
+            </Button>
           </div>
         </div>
       </div>
